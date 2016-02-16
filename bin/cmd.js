5cec1895902dbbfbcb1e14fe4be1eb200eff165c
Dan Ambrisco <dambrisco@gmail.com>
2016-02-16 11:34:02 -0700

    Revert "Merge remote-tracking branch 'substack/master'"
    This reverts commit 4e666af41dd762d7111e1acab616c1211589e0db, reversing
    changes made to 15fb4fdcbe4c33f4f005e5ab31949df143db5739.

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,8 +5,7 @@ var path = require('path');
 var hyperquest = require('hyperquest');
 var stream = require('stream');
 var concat = require('concat-stream');
-var rsa = require('hybrid-rsa-stream');
-var level = require('level');
+var rsa = require('rsa-stream');
 var HOME = process.env.HOME || process.env.USERPROFILE;
 
 var minimist = require('minimist');
